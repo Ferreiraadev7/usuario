b3af6816ab49100cdbe9b3712e84aa399df5fa06
Rafael Ferreira <ferreirarafah1277@gmail.com>
2025-12-19 13:14:14 -0300

    Revert "Feature/requisicoes"

--- src/main/java/com/example/usuario/infrastructure/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.usuario.infrastructure.repository;
-
-import com.example.usuario.infrastructure.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
